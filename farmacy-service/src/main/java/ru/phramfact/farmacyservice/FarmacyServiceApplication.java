803898a11add7ca80d3e8f4180a67f8c4dedea10
Iakov Lysenko <lysenko_iakov@yahoo.com>
2025-11-05 19:50:01 +0300

    init

---
+++ farmacy-service/src/main/java/ru/phramfact/farmacyservice/FarmacyServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.phramfact.farmacyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmacyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FarmacyServiceApplication.class, args);
+    }
+
+}
